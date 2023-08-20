85172ce8a7f73929a7cba8f40866e76a82d9079c
Juliana <julianatsoc28@gmail.com>
2023-08-20 20:47:45 -0300

    before pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,19 @@
-function convertPokemonTypestoLi(pokemonTypes) {
-  return pokemonTypes.map((typeSlot) => `<li class="item-type">${typeSlot.type.name}  </li> `)
-}
 
 function convertPokemonToLi(pokemon) {
   return `
 
-    <li class="pokemon" id="firstColor">
+    <li class="pokemon ${pokemon.type}" id="firstColor">
           <span class="number">#${pokemon.id}</span>
           <div class="name">${pokemon.name}</div>
           <div class="details-types">
             <ol class="list-types">
-              ${convertPokemonTypestoLi(pokemon.types).join('')}
-            </ol>
-            <div class="image">
-              <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${pokemon.id}.png" alt="${pokemon.name}" />
-            </div>
-        </li>
-    `
+              ${pokemon.types.map((type) => `<li class="item-type">${type}</li> `).join('')}
+            </ol >
+  <div class="image">
+    <img src="${pokemon.photo}" alt="${pokemon.name}" />
+  </div>
+        </li >
+  `
 }
 
 const pokemonList = document.getElementById('pokemonList');
@@ -25,3 +22,42 @@ pokeApi.getPokemons().then((pokemons = []) => {
   const newHtml = pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
   pokemonList.innerHTML = newHtml
 })
+
+function applyTypeColorToElement(element, type) {
+  const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
+
+  const backgroundColorHex = typeColors[type] || "#000000";
+  const backgroundColor = `rgba(${parseInt(backgroundColorHex.slice(1, 3), 16)}, ${parseInt(backgroundColorHex.slice(3, 5), 16)}, ${parseInt(backgroundColorHex.slice(5, 7), 16)}, 0.55)`;
+  element.style.backgroundColor = backgroundColor;
+}
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newHtml = pokemons.map(convertPokemonToLi).join('');
+  pokemonList.innerHTML = newHtml;
+
+  const elementsToApplyColor = document.querySelectorAll('.pokemon, .item-type');
+  elementsToApplyColor.forEach((element) => {
+    const elementType = element.classList.contains('pokemon') ? element.classList[1] : element.textContent.toLowerCase();
+    applyTypeColorToElement(element, elementType);
+  });
+});
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,16 +1,30 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.id = pokeDetail.id
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
 
+    pokemon.types = types
+    pokemon.type = type
+
+    return pokemon
+}
 // Abstrai o consumo do HTTP, faz o get e da o result pronto dos pokemons
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then(response => response.json())
+    return fetch(pokemon.url)
+        .then(response => response.json())
+        .then(convertPokeApiDetailToPokemon)
 
 
 }
 
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
