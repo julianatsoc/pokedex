ff009243ad0efd47a860c4340ae74105bf2d2e6b
Juliana <julianatsoc28@gmail.com>
2023-08-20 17:49:54 -0300

    already done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
-// const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-// const offset = 0;
-// const limit = 10;
+function convertPokemonTypestoLi(pokemonTypes) {
+  return pokemonTypes.map((typeSlot) => `<li class="item-type">${typeSlot.type.name}  </li> `)
+}
 
-// fetch(url).then(function(response){
-//     console.log('response');
-// });
+function convertPokemonToLi(pokemon) {
+  return `
 
-// const x = 10 + 10;
-// console.log(x);
+    <li class="pokemon" id="firstColor">
+          <span class="number">#${pokemon.id}</span>
+          <div class="name">${pokemon.name}</div>
+          <div class="details-types">
+            <ol class="list-types">
+              ${convertPokemonTypestoLi(pokemon.types).join('')}
+            </ol>
+            <div class="image">
+              <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${pokemon.id}.png" alt="${pokemon.name}" />
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newHtml = pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
+  pokemonList.innerHTML = newHtml
+})
---
+++ assets/js/poke-api.js
@@ -0,0 +1,23 @@
+const pokeApi = {}
+
+
+// Abstrai o consumo do HTTP, faz o get e da o result pronto dos pokemons
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then(response => response.json())
+
+
+}
+
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
+}
