05c1a1883766fc07c1eb1d21f606a93d264a7ed5
Juliana <julianatsoc28@gmail.com>
2023-08-19 21:35:48 -0300

    layout of poxedex

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+// const offset = 0;
+// const limit = 10;
+
+// fetch(url).then(function(response){
+//     console.log('response');
+// });
+
+// const x = 10 + 10;
+// console.log(x);
